a5f1fd1569c4c987034ba5942d82be58f9d350ad
Akio Tomita <aki@nyan.nyanta.jp>
2019-09-09 16:17:40 +0900

    update README / put pdf2jpg, but not completed

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pdf2jpg
 It can be convert PDF to JPG by drag and drop on batch file.
+
+## What's this.
+
+It provides easy convert method by drag and drop to batch file.
+You should require install ImageMagick on Windows 10.
+
+### What's required.
+
+1. Windows 10.
+1. ImageMagick.
---
+++ pdf2jpg.ps1
@@ -0,0 +1,21 @@
+﻿# SPDX-License-Identifier: MIT
+# Copyright (C) 2019 Akio Tomita
+
+#=============================================================
+# v 0.0α 2019/09/09
+#=============================================================
+
+#=============================================================
+# Settings (please change this config.)
+#=============================================================
+#ImageMagick
+[string]$imagickConvertExe = "C:\ImageMagick\convert.exe"
+
+
+foreach($arg in $args)
+{
+    #$desktopPath = [Environment]::GetFolderPath('Desktop')
+    $destinationFile = [System.IO.Path]::ChangeExtension($arg, ".jpg")
+    Start-Process $imagickConvertExe -ArgumentList "-geometry 3840x2160 -density 300 -quality 95 $arg -background white -flatten -alpha off "  , $destinationFile
+
+}
